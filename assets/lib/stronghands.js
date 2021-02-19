3297cb95386546ee073451ef49c0ead7903ae878
MRFUNCT1ON <69618397+MRFUNCT1ON@users.noreply.github.com>
2021-02-20 02:55:56 +1000

    gauntlet mode

--- assets/lib/stronghands.js
+++ assets/lib/stronghands.js
@@ -26,242 +26,11 @@ let el = function (id) {
     return document.querySelector(id);
 };
 
-let strongHandsManagerAddress = '0x4224d2B1cCE1285C298582b6b735ccFd14109ea1';
-let strongHandsManagerContract = web3.eth.contract([{
-    "constant": true,
-    "inputs": [],
-    "name": "myStrongHand",
-    "outputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "isStrongHand",
-    "outputs": [{
-        "name": "",
-        "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{
-        "name": "_unlockAfterNDays",
-        "type": "uint256"
-    }],
-    "name": "create",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "name": "strongHands",
-    "outputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "anonymous": false,
-    "inputs": [{
-        "indexed": true,
-        "name": "owner",
-        "type": "address"
-    }, {
-        "indexed": true,
-        "name": "strongHand",
-        "type": "address"
-    }],
-    "name": "CreatedStrongHand",
-    "type": "event"
-}]);
+let strongHandsManagerAddress = '0x7f922e7313ac76bb3eD4B32Fd82B1aC482704984';
+let strongHandsManagerContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"_unlockAfterNDays","type":"uint256"}],"name":"create","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"strongHand","type":"address"}],"name":"CreatedGauntlet","type":"event"},{"constant":true,"inputs":[],"name":"isGodlyChad","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myGauntlet","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"strongHands","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]);
 let strongHandsManagerInstance = strongHandsManagerContract.at(strongHandsManagerAddress);
 
-strongHandContract = web3.eth.contract([{
-    "constant": true,
-    "inputs": [],
-    "name": "creationDate",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "withdrawDividends",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "buyWithBalance",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "withdraw",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{
-        "name": "_howManyDays",
-        "type": "uint256"
-    }],
-    "name": "extendLock",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "unlockAfterNDays",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "balanceOf",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{
-        "name": "",
-        "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "isLocked",
-    "outputs": [{
-        "name": "",
-        "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "buy",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{
-        "name": "_toAddress",
-        "type": "address"
-    }, {
-        "name": "_amountOfTokens",
-        "type": "uint256"
-    }],
-    "name": "transfer",
-    "outputs": [{
-        "name": "",
-        "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "lockedUntil",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [{
-        "name": "_amount",
-        "type": "uint256"
-    }],
-    "name": "sell",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "constant": true,
-    "inputs": [],
-    "name": "dividendsOf",
-    "outputs": [{
-        "name": "",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "constant": false,
-    "inputs": [],
-    "name": "reinvest",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "inputs": [{
-        "name": "_owner",
-        "type": "address"
-    }, {
-        "name": "_unlockAfterNDays",
-        "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-}, {
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "fallback"
-}]);
+strongHandContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"creationDate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdrawDividends","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"buyWithBalance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_howManyDays","type":"uint256"}],"name":"extendLock","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"unlockAfterNDays","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isLocked","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"buy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_toAddress","type":"address"},{"name":"_amountOfTokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"developer","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lockedUntil","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_amount","type":"uint256"}],"name":"sell","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"dividendsOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"reinvest","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_owner","type":"address"},{"name":"_unlockAfterNDays","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]);
 let myStrongHandInstance;
 
 function startDapp() {
@@ -270,26 +39,36 @@ function startDapp() {
 }
 
 function myStrongHand() {
-    strongHandsManagerInstance.isStrongHand((error, isStrongHand) => {
-        if (isStrongHand) {
-            el('#mystronghand').innerHTML = '<table><thead><tr><th>Contract Address</th><th>Balance</th><th>Dividends</th><th>Status</th><th>Total Lock Time (days)</th><th>Created</th></tr></thead><tbody><tr><td id="address"></td><td id="myp3dbalance"></td><td id="mydividends"></td><td id="mystatus"></td><td id="locktime"></td><td id="created"></td></tr></tbody></table>';
+    strongHandsManagerInstance.isGodlyChad((error, isGodlyChad) => {
+        if (isGodlyChad) {
+            el('#mystronghand').innerHTML = '<table><thead><tr><th>Contract</th><th>Balance</th><th>Dividends</th><th>Status</th><th>Days</th><th>Created</th></tr></thead><tbody><tr><td id="address"></td><td id="myp3dbalance"></td><td id="mydividends"></td><td id="mystatus"></td><td id="locktime"></td><td id="created"></td></tr></tbody></table>';
 
-            el('#mystronghand').innerHTML += '<input id="buyamount" type="number" placeholder="Amount to spend (ETH)"/>'
-            el('#mystronghand').innerHTML += ' <button type="button" onclick="buyP3D()">Buy/Lock up P3D</button>'
-            el('#mystronghand').innerHTML += ' <button type="button" onclick="withdrawDividends()">Withdraw Dividends</button>'
-            el('#mystronghand').innerHTML += ' <button type="button" onclick="reinvestDividends()">Reinvest Dividends</button>'
-            el('#mystronghand').innerHTML += ' <input id="extendlocktime" type="number" placeholder="lock time (days)" min="1"step="1"/>'
-            el('#mystronghand').innerHTML += ' <button type="button" onclick="extendLock()">Extend Lock Time</button>'
+            el('#myDepositPanel').innerHTML += '<h3>Deposit BNB &amp; lock B1VS</h3>';
+            el('#myDepositPanel').innerHTML += '<input id="buyamount" type="number" placeholder="Amount to spend (BNB)" class="form-control roundedCorners" /><br />';
+            el('#myDepositPanel').innerHTML += ' <button type="button" onclick="buyP3D()" class="btn btn-md btn-dark btn-block btn-outline-success roundedCorners">Buy/Lock up B1VS</button>';
+            el('#myDepositPanel').innerHTML += ' <br />';
+            el('#myDepositPanel').innerHTML += ' <hr />';
+            
+            
+            el('#myTimePanel').innerHTML += ' <h3>Extend your Time</h3>';
+            el('#myTimePanel').innerHTML += ' <input id="extendlocktime" type="number" placeholder="lock time (days)" min="1"step="1" class="form-control roundedCorners" /><br />';
+            el('#myTimePanel').innerHTML += ' <button type="button" onclick="extendLock()" class="btn btn-md btn-dark btn-block btn-outline-warning roundedCorners">Extend Lock Time</button>';
+            el('#myTimePanel').innerHTML += ' <br />';
+            el('#myTimePanel').innerHTML += ' <hr />';
+            
+            el('#myControlsPanel').innerHTML += ' <h3>Collect &amp; Reinvest</h3>';
+            el('#myControlsPanel').innerHTML += ' <button type="button" onclick="reinvestDividends()" class="btn btn-md btn-dark btn-block btn-outline-info roundedCorners">Reinvest Dividends</button>';
+            el('#myControlsPanel').innerHTML += ' <button type="button" onclick="withdrawDividends()" class="btn btn-md btn-dark btn-block btn-outline-danger roundedCorners">Withdraw Dividends</button>';
 
-            strongHandsManagerInstance.myStrongHand((error, myStrongHandAddress) => {
+            strongHandsManagerInstance.myGauntlet((error, myStrongHandAddress) => {
                 el('#address').innerHTML = myStrongHandAddress;
 
                 myStrongHandInstance = strongHandContract.at(myStrongHandAddress);
                 myStrongHandInstance.balanceOf((error, myP3dBalance) => {
-                    el('#myp3dbalance').innerHTML = web3.fromWei(myP3dBalance, 'ether').toFixed(6) + ' P3D';
+                    el('#myp3dbalance').innerHTML = web3.fromWei(myP3dBalance, 'ether').toFixed(6) + ' B1VS';
                 });
                 myStrongHandInstance.dividendsOf((error, myDividends) => {
-                    el('#mydividends').innerHTML = web3.fromWei(myDividends, 'ether').toFixed(6) + ' ETH';
+                    el('#mydividends').innerHTML = web3.fromWei(myDividends, 'ether').toFixed(6) + ' BNB';
                 });
                 myStrongHandInstance.isLocked((error, isLocked) => {
                     if (isLocked) {
@@ -297,9 +76,10 @@ function myStrongHand() {
                             el('#mystatus').innerHTML = '<b>LOCKED UNTIL</b> ' + new Date(lockedUntil * 1000);
                         });
                     } else {
-                        el('#mystatus').innerHTML = '<b>UNLOCKED</b>';
-                        el('#mystronghand').innerHTML += ' <input id="sellamount" type="number" placeholder="Amaount to sell (P3D)"/>'
-                        el('#mystronghand').innerHTML += ' <button type="button" onclick="sell()">Sell P3D</button>'
+                        el('#mySellPanel').innerHTML = '<h3 class="text-success">UNLOCKED</h3>';
+                        el('#mySellPanel').innerHTML += ' <strong>Sell B1VS</strong>';
+                        el('#mySellPanel').innerHTML += ' <input id="sellamount" type="number" placeholder="Amount to Sell (B1VS)" class="form-control roundedCorners" />';
+                        el('#mySellPanel').innerHTML += ' <button type="button" onclick="sell()">Sell B1VS</button>';
                     }
                 });
 
@@ -312,8 +92,8 @@ function myStrongHand() {
             });
 
         } else {
-            el('#mystronghand').innerHTML = '<input id="locktime" type="number" placeholder="LOCK TIME (days)" min="0"step="1"/>'
-            el('#mystronghand').innerHTML += '<button type="button" onclick="getStrong()">Become a Strong Hand!</button>'
+            el('#setupPanel').innerHTML = '<h3>Create a Gauntlet</h3><input id="locktime" type="number" placeholder="LOCK TIME (days)" min="0"step="1" class="form-control roundedCorners"  /><br />'
+            el('#setupPanel').innerHTML += '<button type="button" onclick="getStrong()" class="btn btn-block btn-md btn-dark btn-outline-success roundedCorners">Become a Strong Hand!</button>'
         }
     });
 }
@@ -329,7 +109,7 @@ const promisify = (inner) =>
     );
 
 async function searchStrongHands() {
-    const events = await promisify(eventResult => strongHandsManagerInstance.CreatedStrongHand({}, {
+    const events = await promisify(eventResult => strongHandsManagerInstance.CreatedGauntlet({}, {
         fromBlock: 6476646,
         toBlock: 'latest'
     }).get(eventResult));
